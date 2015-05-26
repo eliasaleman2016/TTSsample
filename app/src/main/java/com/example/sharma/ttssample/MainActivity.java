288582b54952d9c5959f9dccc56c807ec77c00ef
gvsharma <gvsharma4@gmail.com>
2015-05-26 23:06:35 +0530

    This includes changing the main activity with useful code and i have removed all the unncessary code, also arranged some useful comments. added string which will show to user when the app opens and changed button text and action event for button Thanks.

--- app/src/main/java/com/example/sharma/ttssample/MainActivity.java
+++ app/src/main/java/com/example/sharma/ttssample/MainActivity.java
@@ -26,15 +26,24 @@ import java.util.Locale;
 
 /**
  * Created by sharma on 24/5/15.
+ * this class have several attempts to capture users voise
  */
 public class MainActivity extends ActionBarActivity {
 
-    private final int REQ_CODE_SPEECH_INPUT = 100;
+    //This class provides access to the speech recognition service
     private SpeechRecognizer mSpeechRecognizer;
     private Intent mSpeechRecognizerIntent;
-    private boolean mIslistening;
+
+    //checking whether the system have ability to listen
+    private boolean islistening;
+
+    //handler to repeat the task(listening)
     private Handler handler;
+
+    //this is for handler
     private Runnable runnable;
+
+    //boolean flag to start the task(listening) depending on the result
     private  boolean isItReady;
 
     @Override
@@ -56,13 +65,16 @@ public class MainActivity extends ActionBarActivity {
         mSpeechRecognizer.setRecognitionListener(listener);
 
        if (mSpeechRecognizer.isRecognitionAvailable(this)) {
-            mIslistening = true;
+            islistening = true;
         }
 
+        /**
+         * here am repeating handler for every  seconds, you can change this according your need. thanks
+         */
         runnable = new Runnable() {
             @Override
             public void run() {
-                if (mIslistening && isItReady) {
+                if (islistening && isItReady) {
                     mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
                     isItReady = false;
                 }
@@ -77,73 +89,19 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
+    //button click event
     public void apply(View view) {
         String str = mSpeechRecognizer.isRecognitionAvailable(this) ? "Yes" : "No";
-        showToastMessage("is this device supports: " + str);
-        if (mIslistening) {
-            mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
+        showToastMessage("is this device supports voice recognition: " + str);
+        if (islistening) {
+           // mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
         }
     }
-
-    public void speak(View view) {
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getClass().getPackage().getName());
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
-        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 10);
-        startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
-    }
-
-
 
     //toast message
     void showToastMessage(String message){
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
-
-    /**
-     * Showing google speech input dialog
-     * */
-    private void promptSpeechInput() {
-        Intent intent = new Intent(RecognizerIntent.ACTION_VOICE_SEARCH_HANDS_FREE);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
-                "my speech");
-        try {
-            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
-        } catch (ActivityNotFoundException a) {
-            Toast.makeText(getApplicationContext(),
-                    "Not supported",
-                    Toast.LENGTH_SHORT).show();
-        }
-    }
-
-
-    // Create an intent that can start the Speech Recognizer activity
-    public void displaySpeechRecognizer(View view) {
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-// Start the activity, the intent will be populated with the speech text
-        startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
-    }
-
-    // This callback is invoked when the Speech Recognizer returns.
-// This is where you process the intent and extract the speech text from the intent.
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode,
-                                    Intent data) {
-        if (requestCode == REQ_CODE_SPEECH_INPUT && resultCode == RESULT_OK) {
-            List<String> results = data.getStringArrayListExtra(
-                    RecognizerIntent.EXTRA_RESULTS);
-            String spokenText = results.get(0);
-            // Do something with spokenText
-            showToastMessage(spokenText);
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
 
     @Override
     protected void onDestroy() {
@@ -158,6 +116,7 @@ public class MainActivity extends ActionBarActivity {
 
     /**
      * Created by sharma on 24/5/15.
+     * This is the class with which we will get required out put results from RecognitionListener
      */
     public class SpeechRecognitionListener implements RecognitionListener {
 
